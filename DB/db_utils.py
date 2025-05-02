79dcd8b49d056a3fd6cae8a8245a9741f1ac61c9
NoraRV <norarv99@gmail.com>
2025-05-02 19:14:21 +0100

    more up-to-date branch: added UserProgressTracker class db_utils (previous branch was too behind)

--- DB/db_utils.py
+++ DB/db_utils.py
@@ -51,3 +51,64 @@ class ExerciseDB:
             if db_connection:
                 db_connection.close()
 
+
+#--------------Nora's code-----------
+"""
+The UserProgressTracker class interacts with the database to fetch workout data and
+calculate and compare it between 2 periods of time.
+
+Tracks the progress by comparing the last 7 days of workouts vs the previous
+7-day period. It helps see improvements in amount of workouts done and
+total time worked out.
+"""
+class UserProgressTracker:
+    def __init__(self):
+        self.connector = get_connection()
+
+    # Handles user progress claculations on demand.
+    def get_7day_progress(self, user_id):
+        db_connection = None
+
+        try:
+            db_connection = self.connector.get_connection()
+            cur = db_connection.cursor()
+            print("Connected to DB") #Debug message
+
+            # SQL Query to fetch data from the workout_log table
+            query = """ 
+                SELECT (
+                    (SELECT COUNT(*) FROM workout_log 
+                    WHERE user_id = %s AND log_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AS recent_workouts,
+                    
+                    (SELECT COALESCE(SUM(duration_minutes), 0) FROM workout_log
+                    WHERE user_id = %s AND log_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AS recent_minutes,
+                                    
+                    (SELECT COUNT(*) FROM workout_log 
+                    WHERE user_id = %s AND log_date BETWEEN DATE_SUB(CURDATE(), INTERVAL 14 DAY)
+                    AND DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AS previous_workouts,
+
+                    (SELECT COALESCE(SUM(duration_minutes), 0) FROM workout_log
+                    WHERE user_id = %s AND log_date BETWEEN DATE_SUB(CURDATE(), INTERVAL 14 DAY)
+                    AND DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AS previous_minutes 
+                )
+                """
+            
+            # user_id 4 times because SQL query has 4 subqueries, each needing a parameter user_id.
+            cur.execute(query, (user_id, user_id, user_id, user_id))
+            recent_w, recent_m, previous_w, previous_m = cur.fetchone()
+
+            # Put the result of the query into a dictionary with 4 variabes
+            result = {
+                'current_workouts': recent_w,
+                'current_minutes': recent_m,
+                'workout_difference': recent_w - previous_w,
+                'minutes_difference': recent_m - previous_m
+            }
+            cur.close()
+
+            return result
+        
+        finally:
+            if db_connection:
+                db_connection.close()
+                print("DB connection is closed")#Debug message
